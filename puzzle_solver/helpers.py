e9f6347b5c3edca3d5dc17a137bb5ec488f39a70
riho209 <46776175+riho209@users.noreply.github.com>
2021-10-05 17:59:15 +0900

    helpersの変更
    sort_puzzleに渡すtxtファイルの修正
    make_dummy関数の回転情報の間違い修正

--- puzzle_solver/helpers.py
+++ puzzle_solver/helpers.py
@@ -1,18 +1,20 @@
 import numpy as np
 import codecs
 from PIL import Image
-
+#回転情報だけあっているダミーの情報
 def make_solution(save_path,Place_mat,Rotmat,puzzle_inf):
     width,height=puzzle_inf[:2]
-    solution=[str(format(i,'x'))+str(format(j,'x')) for i in range(height)  for j in range(width)]
+    solution=[str(format(j,'x'))+str(format(i,'x')) for i in range(height)  for j in range(width)]
     solution=np.reshape(solution,(height,width))
+
     rot_mat=np.zeros((height,width),dtype=np.int32)
+
     for i in range(height):
         for j in range(width):
             piece1=Place_mat[i,j]//width
             piece2=Place_mat[i,j]%width
-            solution[i,j]=str(format(piece1,'x'))+str(format(piece2,'x'))
-            rot_mat[i,j]=Rotmat[i,j]
+            solution[piece1,piece2]=str(format(j,'x'))+str(format(i,'x'))
+            rot_mat[piece1,piece2]=Rotmat[i,j]
     #print(solution)
     #print(rot_mat)
     text_file=open(save_path+"puzzle_solution.txt","wt")
@@ -42,7 +44,7 @@ def take_problem_inf(problem_path):
             elif num==1:
                 return str_inf[2:len(str_inf)-1]
             
-    with codecs.open(problem_path,'r',encoding='utf-8') as f:
+    with codecs.open(problem_path,'r',encoding='Shift_JIS') as f:
         line=f.readline()
         divide=f.readline()
         select_count=f.readline()
@@ -50,22 +52,38 @@ def take_problem_inf(problem_path):
         width,height=splite(divide,2)[:2]
         select_count=splite(select_count,1)
         select_rate,change_rate=splite(rate,2)[:2]
-        #print(width,height)
-        #print(select_count)
-        #print(select_rate,change_rate)
+
         f.close()
     return [int(width),int(height),int(select_count),int(select_rate),int(change_rate)]
 def problem_ppm_png(input_path,output_path):
     im=Image.open(input_path)
     im.show()
     im.save(output_path)
-def make_dummy(save_path,rot_mat,puzzle_inf):
+
+#回転情報だけあっているダミーの情報
+def make_dummy(save_path,Place_mat,rot_mat,puzzle_inf):
     text_file=open(save_path+"solution.txt","wt")
     width,height=puzzle_inf[:2]
+    rot_buf=rot_mat.flatten()
+    rot_inf=np.zeros((width*height),dtype=np.int32)
     for i in range(height):
         for j in range(width):
-            text_file.write(str(rot_mat[i,j]))
+            rot_inf[Place_mat[i,j]]=rot_mat[i,j]
+    for i in range(width*height):
+        text_file.write(str(rot_inf[i]))
     text_file.write("\n")
-    text_file.write('0')
+    text_file.write("1")
+    text_file.write("\n")
+    text_file.write("00")
+    text_file.write("\n")
+    text_file.write("1")
+    text_file.write("\n")
+    text_file.write("R")
+
     print("疑似解答保存")
     text_file.close()
+
+'''
+if __name__=='__main__':
+    make_solution("D:/Python_Script/puzzle_text")
+'''
