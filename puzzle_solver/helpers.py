f5ca6e3f4fdd60e1c63f70595b359e02121af4ed
riho209 <46776175+riho209@users.noreply.github.com>
2021-10-08 15:04:52 +0900

    変更

--- puzzle_solver/helpers.py
+++ puzzle_solver/helpers.py
@@ -15,26 +15,33 @@ def make_solution(save_path,Place_mat,Rotmat,puzzle_inf):
             piece2=Place_mat[i,j]%width
             solution[piece1,piece2]=str(format(j,'x'))+str(format(i,'x'))
             rot_mat[piece1,piece2]=Rotmat[i,j]
-    #print(solution)
-    #print(rot_mat)
+    
     text_file=open(save_path+"puzzle_solution.txt","wt")
     text_file.write(str(width)+" "+str(height)+"\n")
     text_file.write(str(puzzle_inf[2])+"\n")
     text_file.write(str(puzzle_inf[3])+" "+str(puzzle_inf[4])+"\n")
 
     for i in range(height):
-            for j in range(width):
-                    text_file.write(solution[i,j])
-                    if j!=width-1:
-                        text_file.write(" ")
-            text_file.write("\n")
+        for j in range(width):
+                text_file.write(solution[i,j])
+                if j!=width-1:
+                    text_file.write(" ")
+        if i!=height-1:            
+            text_file.write(" ")
+    
+    text_file.write("\n")
+    #回転情報追加
+    rot_inf=np.zeros((width*height),dtype=np.int32)
     for i in range(height):
-            for j in range(width):
-                    text_file.write(str(rot_mat[i,j]))
-                    if j!=width-1:
-                        text_file.write(" ")
-            text_file.write("\n")
+        for j in range(width):
+            rot_inf[Place_mat[i,j]]=rot_mat[i,j]
+    for i in range(width*height):
+        text_file.write(str(rot_inf[i]))
+    text_file.write("\n")
     text_file.close()
+    print("解答")
+    print(solution)
+    print(rot_mat)
 
 def take_problem_inf(problem_path):
     def splite(str_inf,num):
